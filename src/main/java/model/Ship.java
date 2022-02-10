7d7245b5503d1e1fad49a59b1ccc96ea7162ddf8
Michal Milewski <michal.milewski98@gmail.com>
2022-02-10 17:09:17 +0100

    Add methods checking if ship is sunk or hit

--- src/main/java/model/Ship.java
+++ src/main/java/model/Ship.java
@@ -1,5 +1,7 @@
 package model;
 
+import model.Coordinates;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +19,45 @@ public class Ship {
         this.horizontal = horizontal;
         this.isSunk = false;
         this.coordinates = new ArrayList<>();
+    }
+
+    public boolean isSunk() {
+        for (model.Coordinates coordinate : coordinates) {
+            if (coordinate.getStatus().equals(model.Status.NOT_HIT)) {
+                return false;
+            }
+        }
+        this.setSunk(true);
+        return true;
+    }
+
+    public boolean isHit(int x, int y) {
+        return coordinates.stream().anyMatch(c -> c.getX() == x &&
+                c.getY() == y &&
+                c.getStatus() == Status.NOT_HIT);
+    }
+
+    public List<Coordinates> getCoordinates() {
+        return this.coordinates;
+    }
+
+    public void addCoordinate(Coordinates coordinate) {
+        this.coordinates.add(coordinate);
+    }
+
+    public boolean isHorizontal() {
+        return this.horizontal;
+    }
+
+    public void setSunk(boolean sunk) {
+        this.isSunk = sunk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxEndingIndex(int endIndex, int length) {
+        return endIndex + length - 1;
     }
 }
